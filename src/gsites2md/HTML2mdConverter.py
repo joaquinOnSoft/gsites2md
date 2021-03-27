1398c0d06433f708d5ddec3bb58357c42a1ecc26
Joaquín Garzón <joaquingp@gmail.com>
2021-03-27 09:27:06 +0100

    HTML 2 Markdown conversion improved

--- src/gsites2md/HTML2mdConverter.py
+++ src/gsites2md/HTML2mdConverter.py
@@ -6,91 +6,122 @@ class HTML2mdConverter(HTMLParser):
         super().__init__()
         self.reset()
         self.md = ""
+        self.nested_list = []
 
     def handle_starttag(self, tag, attrs):
-        switcher = {
-            "img": HTML2mdConverter.img(attrs)
-        }
-        html2md = switcher.get(self.lasttag, HTML2mdConverter.default())
+        html2md = ""
+        if tag == "img":
+            html2md = HTML2mdConverter.img(attrs)
+        elif tag == "ul":
+            self.__push_nested_list(tag)
+        elif tag == "l":
+            self.__push_nested_list(tag)
+
         self.md += html2md
 
     def handle_endtag(self, tag):
-        # print("Encountered an end tag :", tag)
-        pass
+        if tag == "ul":
+            self.__pop_nested_list(tag)
+        elif tag == "l":
+            self.__pop_nested_list(tag)
 
     def handle_data(self, data):
-        switcher = {
-            "h1": HTML2mdConverter.h1(data),
-            "h2": HTML2mdConverter.h2(data),
-            "h3": HTML2mdConverter.h3(data),
-            "h4": HTML2mdConverter.h4(data),
-            "h5": HTML2mdConverter.h5(data),
-            "h6": HTML2mdConverter.h6(data),
-            "strong": HTML2mdConverter.strong(data),
-            "ul": HTML2mdConverter.ul(data),
-            "ol": HTML2mdConverter.ol(data),
-            "li": HTML2mdConverter.li(data),
-        }
-        html2md = switcher.get(self.lasttag, HTML2mdConverter.default())
-        self.md += html2md
+        if data.replace(" ", "") != "\n":
+            switcher = {
+                "h1": HTML2mdConverter.h1(data),
+                "h2": HTML2mdConverter.h2(data),
+                "h3": HTML2mdConverter.h3(data),
+                "h4": HTML2mdConverter.h4(data),
+                "h5": HTML2mdConverter.h5(data),
+                "h6": HTML2mdConverter.h6(data),
+                "h7": HTML2mdConverter.h7(data),
+                "h8": HTML2mdConverter.h8(data),
+                "strong": HTML2mdConverter.strong(data),
+                "li": self.li(data)
+            }
+            html2md = switcher.get(self.lasttag, HTML2mdConverter.default(data))
+            self.md += html2md
 
     def error(self, message):
         pass
 
     @staticmethod
     def h1(data: str) -> str:
-        return "# " + data + "\n"
+        return "\n# " + data + "\n"
 
     @staticmethod
     def h2(data: str) -> str:
-        return "## " + data + "\n"
+        return "\n## " + data + "\n"
 
     @staticmethod
     def h3(data: str) -> str:
-        return "### " + data + "\n"
+        return "\n### " + data + "\n"
 
     @staticmethod
     def h4(data: str) -> str:
-        return "#### " + data + "\n"
+        return "\n#### " + data + "\n"
 
     @staticmethod
     def h5(data: str) -> str:
-        return "##### " + data + "\n"
+        return "\n##### " + data + "\n"
 
     @staticmethod
     def h6(data: str) -> str:
-        return "###### " + data + "\n"
+        return "\n###### " + data + "\n"
+
+    @staticmethod
+    def h7(data: str) -> str:
+        return "\n####### " + data + "\n"
+
+    @staticmethod
+    def h8(data: str) -> str:
+        return "\n######## " + data + "\n"
 
     @staticmethod
     def strong(data: str) -> str:
         return "**" + data + "**"
 
-    @staticmethod
-    def ul(data: str) -> str:
-        return "* " + data + "\n"
+    def __push_nested_list(self, tag: str):
+        self.nested_list.append(tag)
 
-    @staticmethod
-    def ol(data: str) -> str:
-        return "1. " + data + "\n"
+    def __pop_nested_list(self, tag: str) -> str:
+        return self.nested_list.pop()
 
-    @staticmethod
-    def li(data: str) -> str:
-        return "* " + data + "\n"
+    def li(self, data: str) -> str:
+        size = len(self.nested_list)
+        if size > 0:
+            filler = ""
+            for step in range(size):
+                filler += "   "
+
+            last_list_tag = self.nested_list[-1]
+            if last_list_tag == "ul":
+                return filler + "* " + data + "\n"
+            elif last_list_tag == "ol":
+                return filler + "1. " + data + "\n"
+        else:
+            return ""
 
     @staticmethod
     def img(attrs) -> str:
         alt = ""
         link = ""
 
-        # TODO refactor: Extract method
-        for name, value in attrs:
-            if name == 'alt':
-                alt = value
-            if name == "src":
-                link = value
-
+        alt = HTML2mdConverter.__get_attribute_by_name(attrs, "alt")
+        link = HTML2mdConverter.__get_attribute_by_name(attrs, "src")
         return f'![{alt}]({link})\n'
 
     @staticmethod
-    def default() -> str:
+    def __get_attribute_by_name(attrs, attr_name):
+        for name, value in attrs:
+            if name == attr_name:
+                return value
         return ""
+
+    @staticmethod
+    def default(data) -> str:
+        return ""
+
+    @staticmethod
+    def do_nothing():
+        pass
--- src/gsites2md/test/TestHTML2mdConverter.py
+++ src/gsites2md/test/TestHTML2mdConverter.py
@@ -6,22 +6,28 @@ from ..HTML2md import HTML2mdConverter
 class TestHTML2mdConverter(TestCase):
 
     def test_h1(self):
-        self.assertEqual("# Hello\n", HTML2mdConverter.h1("Hello"))
+        self.assertEqual("\n# Hello\n", HTML2mdConverter.h1("Hello"))
 
     def test_h2(self):
-        self.assertEqual("## Hello\n", HTML2mdConverter.h2("Hello"))
+        self.assertEqual("\n## Hello\n", HTML2mdConverter.h2("Hello"))
 
     def test_h3(self):
-        self.assertEqual("### Hello\n", HTML2mdConverter.h3("Hello"))
+        self.assertEqual("\n### Hello\n", HTML2mdConverter.h3("Hello"))
 
     def test_h4(self):
-        self.assertEqual("#### Hello\n", HTML2mdConverter.h4("Hello"))
+        self.assertEqual("\n#### Hello\n", HTML2mdConverter.h4("Hello"))
 
     def test_h5(self):
-        self.assertEqual("##### Hello\n", HTML2mdConverter.h5("Hello"))
+        self.assertEqual("\n##### Hello\n", HTML2mdConverter.h5("Hello"))
 
     def test_h6(self):
-        self.assertEqual("###### Hello\n", HTML2mdConverter.h6("Hello"))
+        self.assertEqual("\n###### Hello\n", HTML2mdConverter.h6("Hello"))
+
+    def test_h7(self):
+        self.assertEqual("\n####### Hello\n", HTML2mdConverter.h7("Hello"))
+
+    def test_h8(self):
+        self.assertEqual("\n######## Hello\n", HTML2mdConverter.h8("Hello"))
 
     def test_strong(self):
         self.assertEqual("**Hello**", HTML2mdConverter.strong("Hello"))
