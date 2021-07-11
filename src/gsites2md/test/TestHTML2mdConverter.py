97604cafafe2a819530a2edf5ea413565802eac2
JoaquinOnSoft <joaquingp@gmail.com>
2021-07-11 19:42:54 +0200

    Format file

--- src/gsites2md/test/TestHTML2mdConverter.py
+++ src/gsites2md/test/TestHTML2mdConverter.py
@@ -125,4 +125,4 @@ class TestHTML2mdConverter(TestCase):
 
     def test_comment(self):
         self.assertEqual("[//]: # (This is a comment)",
-                    HTML2mdConverter.comment("This is a comment"))
+                         HTML2mdConverter.comment("This is a comment"))
